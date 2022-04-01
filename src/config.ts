3a41c223b54a8dc037ca37d1314a3bb9d7613532
Kamil Hismatullin <kamil.hismatullin@flatstack.com>
2022-04-01 09:56:53 +0200

    add ptosWidget remote link to config

--- src/config.ts
+++ src/config.ts
@@ -14,4 +14,19 @@ export default [
       height: 10,
     },
   },
+  {
+    id: 'ptosWidget',
+    remoteUrl:
+      'https://ptos-widget.s3.eu-central-1.amazonaws.com/dist/remote.js',
+    remoteName: 'ptosWidget',
+    name: 'PTOs Widget',
+    exposedName: '.',
+    customElement: 'fwd-ptos-widget',
+    position: {
+      x: 15,
+      y: 0,
+      width: 320,
+      height: 320,
+    },
+  },
 ] as const;
